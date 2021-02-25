03d6726bdc199de4055665cfb4c2001c3f6c833d
Igor.Bezdenezhnykh <Igor.Bezdenezhnykh@x5.ru>
2021-02-25 22:37:41 +0300

    added types

--- src/api/beer.ts
+++ src/api/beer.ts
@@ -1,10 +1,34 @@
 import ApiClient from './ApiClient';
 
-
+interface IVolume {
+  value: number;
+  unit: string;
+}
+interface IBeerDTO {
+  id: string;
+  name: string;
+  tagline: string;
+  abv: number;
+  attenuation_level: number;
+  boil_volume: IVolume;
+  brewers_tips: string;
+  contributed_by: string;
+  description: string;
+  ebc: number | null;
+  first_brewed: string;
+  food_pairing: string[];
+  ibu: number | null;
+  image_url: string;
+  ph: number | null;
+  srm: number | null;
+  target_fg: number;
+  target_og: number;
+  volume: IVolume;
+}
 
 const beerApi = {
   get: () => {
-    return ApiClient.get(`/beers`);
+    return ApiClient.get<{data: IBeerDTO[]}>(`/beers`);
   },
 };
 
--- src/models/beer.ts
+++ src/models/beer.ts
@@ -1,7 +1,30 @@
 import { types, Instance } from 'mobx-state-tree';
 
+const VolumeModel = types.model({
+  value: types.number,
+  unit: types.string
+})
+
 const Beer = types.model({
+  id: types.number,
   name: types.string,
+  tagline: types.string,
+  abv: types.number,
+  attenuation_level: types.number,
+  boil_volume: VolumeModel,
+  brewers_tips: types.string,
+  contributed_by: types.string,
+  description: types.string,
+  ebc: types.maybeNull(types.number),
+  first_brewed: types.string,
+  food_pairing: types.array(types.string),
+  ibu: types.maybeNull(types.number),
+  image_url: types.string,
+  ph: types.maybeNull(types.number),
+  srm: types.maybeNull(types.number),
+  target_fg: types.number,
+  target_og: types.number,
+  volume: VolumeModel,
 })
 
 export default Beer;
