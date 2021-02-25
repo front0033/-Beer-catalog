ffb2e0f9e954e0350bb392a738400b282454e0b5
Igor.Bezdenezhnykh <Igor.Bezdenezhnykh@x5.ru>
2021-02-25 14:48:33 +0300

    added antd

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
 
-const MainPage = () => (
-  <div>Main Page</div>
-);
+import './styles.css';
+
+import { Button } from 'antd';
+
+const MainPage = () => {
+  return (
+    <div className='container'>
+      <Button>Кнопка</Button>
+    </div>
+  );
+};
 
 export default MainPage;
